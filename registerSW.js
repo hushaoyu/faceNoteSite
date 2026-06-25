32dda0a06a0423ef3e59a00dd96251c4b0f10831
xiling_hushaoyu <husy@sit.com.cn>
2026-06-25 16:10:36 +0800

    update

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/faceNoteSite/sw.js', { scope: '/faceNoteSite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
